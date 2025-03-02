c19077663da95d0584ffaaedd9912059b817055e
Pubudu Premathilaka <pubuduudara7@gmail.com>
2025-03-02 21:31:31 +0530

    wip

--- src/app/(dashboard)/mentor/page.tsx
+++ src/app/(dashboard)/mentor/page.tsx
@@ -1,5 +1,19 @@
+import BigCalendar from "@/components/BigCalender";
+
 const MentorPage = () => {
-  return <div className="">MentorPage</div>;
+  return (
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">TEST</div>
+    </div>
+  );
 };
 
 export default MentorPage;
